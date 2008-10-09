d249d549f9f1d60cc53fda37231995c78833aebd
andre <andrea.mistrali@gmail.com>
2008-10-09 16:14:34 +0000

    Initial import

---
+++ cambi_uic.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+# Copyright (c)2008 CUBE S.p.A. 
+#  
+#  Author: Andrea Mistrali <andre@cubeholding.com> 
+#  Description: Scarica cambi da UIC e aggiorna DB
+# 
+#  $Id: cambi_uic.py,v 1.1.1.1 2008-01-28 09:38:45 andre Exp $ 
+
+# wget -O uic.csv
+# "http://www.uic.it/UICFEWebroot/QueryOneDateAllCur?lang=ita&rate=0&initDay=06& initMonth=03&initYear=2007&refCur=euro&R1=csv"
+#http://uif.bancaditalia.it/UICFEWebroot/QueryOneDateAllCur?lang=ita&rate=0&init Day=07&initMonth=01&initYear=2008&refCur=euro&R1=csv
+
+__version__="$Revision: 1.1.1.1 $"[11:-2]
+
+
+import psycopg2 as db
+import time
+import datetime
+import urllib2
+import csv
+import getopt, sys
+
+currency={'USD': 2, 'GBP': 3}
+baseUrl="http://uif.bancaditalia.it/UICFEWebroot/QueryOneDateAllCur?lang=ita&rate=0&initDay=%02d&initMonth=%02d&initYear=%4d&refCur=euro&R1=csv"
+
+DB="gv"
+HOST="localhost"
+USER="root"
+PWD="chpwd"
+
+DSN="dbname=%s user=%s password=%s host=%s" % (DB, USER, PWD, HOST)
+
+OUTFILE="/tmp/newUic%s.csv"
+
+def usage():
+    print """
+    %s [-n|--nofile] -d|--date "data" - Scarica i cambi di un solo giorno. Per default ieri.
+    %s [-n|--nofile] -s|--start "dataInizio" [-e|--end "dataFine"] - Scarica i cambi a partire da dataInizio, fino a dataFine. dataFine è, per default, ieri.
+
+    Il flag -n fa si che i dati non vengano salvati in un file.
+
+    Tutte le date vanno indicate nel formato "dd/mm/yyyy".
+    """ % (sys.argv[0], sys.argv[0])
+
+def download(date):
+    """
+    Scarica il file relativo ad una certa data e rende il file parserato
+    date è un array [dd,mm,yyyy]
+    """
+    url=baseUrl % (date[0], date[1], date[2])
+    print "Scarico da %s" % url
+    f=urllib2.urlopen(url)
+    t=f.readlines()
+    reader=csv.reader(t)
+    return reader
+
+def insertOrUpdate(date, cod1, cod2, cambio):
+    dataDict={'date': date,
+          'cod1': cod1,
+          'cod2': cod2,
+          'cambio': cambio}
+    
+    selQuery="""SELECT cod_cambio FROM cambio_new where data_cambio='%(date)s' and 
+    ((cod_cambio1=%(cod1)d and cod_cambio2=%(cod2)d) or
+    (cod_cambio1=%(cod2)d and cod_cambio2=%(cod1)d))""" % dataDict
+
+    insQuery="""INSERT INTO cambio_new (data_cambio, cod_cambio1, cod_cambio2, cambio)
+    VALUES ('%(date)s', %(cod1)d, %(cod2)d, %(cambio).5f)""" % dataDict
+
+    updQuery="""UPDATE cambio_new SET cambio=%(cambio).5f WHERE
+    cod_cambio1=%(cod1)d AND
+    cod_cambio2=%(cod2)d AND
+    data_cambio='%(date)s'""" % dataDict
+    
+
+
+    # Connettiamoci al DB
+    connection=db.connect(DSN)
+    cursor=connection.cursor()
+    cursor.execute(selQuery)
+    
+    if not len(cursor.fetchall()):
+        cursor.execute(insQuery)
+    else:
+        cursor.execute(updQuery)
+    connection.commit()
+
+
+
+
+def main():
+    done=False
+    writeFile=True
+    oneDay=datetime.timedelta(1)
+    
+
+    startDate=0
+    endDate=0
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "d:e:ns:", ["date=", "nofile" "start=", "end="])
+    except getopt.GetoptError:
+        # print help information and exit:
+        usage()
+        sys.exit(2)
+
+      
+    for o, a in opts:
+        if o == "-n":
+            writeFile=False
+        if o == "-h":
+            usage()
+            sys.exit(2)
+        if o == "-d":
+            date=a
+            dateArray=map(int, date.split('/'))
+            startDate=datetime.date(dateArray[2], dateArray[1], dateArray[0])
+            endDate=startDate
+        if o == "-s":
+            date=a
+            dateArray=map(int, date.split('/'))
+            startDate=datetime.date(dateArray[2], dateArray[1], dateArray[0])
+        if o == "-e":
+            date=a
+            dateArray=map(int, date.split('/'))
+            endDate=datetime.date(dateArray[2], dateArray[1], dateArray[0])
+
+            if endDate and not startDate:
+                usage()
+                sys.exit(2)
+
+    date=time.strftime("%d/%m/%Y", time.gmtime(time.time()-86400)) #Ieri
+    dateArray=map(int, date.split('/'))
+
+    if not startDate:
+        startDate=datetime.date(dateArray[2], dateArray[1], dateArray[0])
+        
+    if not endDate:
+        endDate=datetime.date(dateArray[2], dateArray[1], dateArray[0])
+
+    while startDate <= endDate:
+        print "Scarico i dati del %02d/%02d/%4d" % (startDate.day, startDate.month, startDate.year)
+        done=False
+        if writeFile:
+            strDate="%d%02d%02d" % (startDate.year, startDate.month, startDate.day)
+            of=open(OUTFILE % strDate, 'w+')
+            w=csv.writer(of)
+        
+        dateArray=[startDate.day, startDate.month, startDate.year]
+        reader=download(map(int, dateArray))
+        dbDate="%04d-%02d-%02d" % (dateArray[2], dateArray[1], dateArray[0])
+        for l in reader:
+            if len(l) > 1:
+                if writeFile:
+                    w.writerow(l)
+                done=True
+                if l[2] in currency.keys():
+                    if writeFile:
+                        w.writerow(l)
+                    insertOrUpdate(dbDate, 1, currency[l[2]], float(l[4]))
+        if done:
+            # Inseriamo i cambi fissi
+            insertOrUpdate(dbDate, 1, 1, 1)
+            insertOrUpdate(dbDate, 2, 2, 1)
+            insertOrUpdate(dbDate, 3, 3, 1)
+        
+        if writeFile:
+            of.close()
+        startDate=startDate+oneDay
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ dbmail-sa-learn.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#  
+# Copyright (c)2007 CUBE S.p.A. 
+#  
+#  Author: Andrea Mistrali <andre@cubeholding.com> 
+#  Description: Pass all spam mail to sa-learn
+# 
+#  $Id: dbmail-sa-learn.py,v 1.6 2007-06-20 09:03:12 andre Exp $ 
+#  
+
+__version__="$Revision: 1.6 $"[11:-2]
+
+
+"""
+TODO Parsing del file di configurazione di dbmail per avere i parametri del DB
+TODO Possibilità di lavorare su un'altra mailbox
+"""
+
+import sys
+import getopt
+import os
+import MySQLdb as db
+
+
+HOST="db.cube.lan"
+DB="dbmail"
+USER="dbmail"
+PWD="dbmailpwd"
+SAMBX="SpamTrain" # SA Mailbox
+HAMBX="SpamNon" # Ham Mailbox
+verbose=False
+dryrun=False
+spamOnly=False
+hamOnly=False
+global connection
+global safd
+
+help_message = """
+This script feeds message to sa-learn, for autolearn of Spam and Ham.
+Options are:
+  -h|--help    show help
+  -n|--dryrun  test, don't delete message
+  -v|--verboes be verbose
+  -H|--ham     only feed Ham
+  -S|--spam    only feed Spam
+ """
+
+class Usage(Exception):
+  def __init__(self, msg):
+    self.msg = msg
+
+def debug(msg):
+  """print msg if verbose=True"""
+  if verbose:
+    print msg
+
+ 
+def feedBlock(msg):
+  """outputs messageblock to sa-learn if dryrun is False, else on stdout"""
+  global safd
+  if not dryrun:
+    safd.write(msg)
+  if verbose:
+    print msg
+
+def getBlocks(messageId):
+  """gets all block of a message and feed them to sa-learn"""
+
+  # Prepare the cursor to retrieve all message blocks
+  messageBlockCursor=connection.cursor()
+  query="""SELECT messageblk FROM dbmail_messageblks WHERE physmessage_id=%s ORDER BY messageblk_idnr""" % messageId
+  messageBlockCursor.execute(query)
+  for messageContent in messageBlockCursor.fetchall():
+    if type(messageContent[0]) == str:
+      text=messageContent[0]
+    else:
+      text=messageContent[0].tostring()
+  return text
+
+def getMailboxID(name=SAMBX, userID=3):
+  """retrieves id of mailbox 'name' owned by user 'userID'"""
+  
+  global connection
+  # retrieve ID of Mailbox SAMBX (SpamTraining Mailbox)
+  query="SELECT mailbox_idnr FROM dbmail_mailboxes WHERE owner_idnr=%d and name='%s'" % (userID,name)
+  debug(query)
+  mbxid=connection.cursor()
+  mbxid.execute(query)
+  (mailboxId,),=mbxid.fetchall()
+  debug("Working on mailbox id %d" % mailboxId)
+  return mailboxId
+
+def getMessagesList(mailboxId):
+  """retrieves list of physmessage_id's of messages in mailbox"""
+  
+  global connection
+  # Select messages from Mailbox 
+  query="""SELECT physmessage_id from dbmail_messages where mailbox_idnr=%s and status < 2 ORDER by physmessage_id """ % mailboxId
+  debug(query)
+  messageCursor=connection.cursor()
+  messageCursor.execute(query)
+  return messageCursor.fetchall()
+
+def delMessage(messageId,mailboxId):
+  """set message with physmessage_id=messageId to deleted status"""
+  
+  global connection
+  # Now we have retrieved message, let's set its status to 2.
+  query="UPDATE dbmail_messages SET status=2,deleted_flag=1 WHERE physmessage_id=%s AND mailbox_idnr=%s;" % (messageId, mailboxId)
+  debug(query)
+  # Prepare the cursor to delete messages (setting status=2 and deleted_flag=1)
+  messageDeleteCursor=connection.cursor()
+  messageDeleteCursor.execute(query)
+  connection.commit() # We need to commit changes!
+
+def getWorkDone():
+  """
+  Select all messages from Spam Engine Train mailbox, feed them to sa-learn and mark them as deleted
+  """
+  global safd
+  global hamOnly
+  global spamOnly
+  for mbx in (SAMBX, HAMBX):
+    if mbx==SAMBX: # Are we working on SPAM or on HAM
+      if (hamOnly):
+        print "hamOnly, skipping %s" % mbx
+        continue
+      safd=os.popen("sa-learn --spam --mbox", 'w')
+    else:
+      if(spamOnly):
+        print "spamOnly, skipping %s" % mbx
+        continue
+      safd=os.popen("sa-learn --ham --mbox", 'w')
+      
+    # Get mailbox ID
+    mailboxId=getMailboxID(mbx)  
+  
+    # Process every message in mailbox
+    for message in getMessagesList(mailboxId):
+      # We need a "From " line at start of message
+      feedBlock("From DBMAIL-EXPORTER-FOR-SA")
+      text=getBlocks(message[0])
+      feedBlock(text) # Feed data to sa-learn
+      feedBlock("\n")
+      if mbx==SAMBX:
+        delMessage(message[0],mailboxId)
+
+    if not dryrun:
+      safd.close()
+      
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv
+  try:
+    try:
+      opts, args = getopt.getopt(argv[1:], "hvnHS", ["help", "verbose","dryrun", "ham", "spam"])
+    except getopt.error, msg:
+      raise Usage("%s\n%s" % (msg,help_message))
+  
+    # option processing
+    for option, value in opts:
+      if option in ("-S", "--spam"):
+        global spamOnly
+        spamOnly = True
+      if option in ("-H", "--ham"):
+        global hamOnly
+        hamOnly = True
+      if option in ("-v", "--verbose"):
+        global verbose
+        verbose = True
+      if option in ("-n", "--dryrun"):
+        global dryrun
+        dryrun = True
+      if option in ("-h", "--help"):
+        raise Usage(help_message)
+   
+  except Usage, err:
+    print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+    return 2
+    
+  # Connect to DB & open connection to sa-learn
+  global connection
+  connection=db.connect(db=DB,host=HOST,user=USER,passwd=PWD)
+ 
+  getWorkDone()
+  
+  # Close connection to DB & to sa-learn
+  connection.close()
+
+if __name__ == "__main__":
+  sys.exit(main())
