ce769e65c770ffbfa5e534e52ea9199365ba3733
andre <andrea.mistrali@gmail.com>
2009-04-29 07:28:16 +0000

    Autocommit - 200904290928 andre -h

--- svnUtils/autoCommit.sh
+++ svnUtils/autoCommit.sh
@@ -8,27 +8,42 @@
 #  $Id$ 
 
 function autoAddDel {
-  svn status | grep '^?' | sed -e 's/^?      /svn add "/g' -e 's/$/"/g'
-  svn status | grep '^!' | sed -e 's/^!      /svn delete "/g' -e 's/$/"/g'
-  msg="Autocommit - `date +%Y%m%d%H%M` $LOGNAME"
-  if [ "$1" != "-n" ]; then
-    echo 'svn commit -m "Autocommit - `date +%Y%m%d%H%M` $LOGNAME"'
-  fi
+    svn status 2>/dev/null| grep '^?' | sed -e 's/^?      /svn add "/g' -e 's/$/"/g'
+    svn status 2>/dev/null| grep '^!' | sed -e 's/^!      /svn delete "/g' -e 's/$/"/g'
+    msg="Autocommit - `date +%Y%m%d%H%M` $LOGNAME"
+    cat<<EOB
+svn commit -m "Autocommit - `date +%Y%m%d%H%M` $LOGNAME $message"
+EOB
 }
 
-if [ "$1" == "-h" ]; then
+if [ '$1' == '-h' ]; then
     cat<<EOF
-`basename $0` [-hni]
+`basename $0` [-hni] message
    -h show help
    -n dryrun
    -i interactive: ask confirmation
+   message Commit message
 EOF
     exit 2
 fi
+dryrun=0
+ask=0
 
-if [ "$1" == "-n" ]; then
-    autoAddDel $1
-elif [ "$1" == "-i" ]; then
+for arg in $*; do
+    if [ "$arg" == '-n' ]; then
+        dryrun=1
+        shift
+    elif [ "$arg" == '-i' ]; then
+        ask=1
+        shift
+    else
+        message=$*
+    fi
+done
+
+if [ $dryrun -eq 1 ]; then
+    autoAddDel
+elif [ $ask -eq 1 ]; then
     echo "Operations:"
     autoAddDel
     echo -n "Confirm [y/n]? "
