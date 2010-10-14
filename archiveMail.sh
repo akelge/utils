99d4ef78ccb31b833d4414403c5e8dd7f984df95
root <akelge@gmail.com>
2010-10-14 14:33:22 +0000

    Changed procedure to archive mail

--- archiveMail.sh
+++ archiveMail.sh
@@ -9,11 +9,10 @@
 # $Id$
 
 LOGFILE="/var/log/archiveMail.log"
-LIST="/usr/local/lib/archiveMail/folders.lst"
 
 SERVER="zeus.oob"
-
-USER="__public__"
+USER1="__public__"
+USER2="__archive__"
 AUTHUSER="dm"
 PASS="chpwd"
 
@@ -22,41 +21,46 @@ if [ "$1" == "-h" ]; then
 
 `basename $0` [-h] [<folder>]
 
-if no folder is specified get folders from:
-    $LIST
+ -h print this help
+
+if no folder is specified archive all #Public folders
 
 EOB
 exit 0
 elif [ "x$1" != "x" ]; then
-    FLIST=$1
+    FLIST="--folder '#Public/$1'"
 else
-    FLIST=`cat $LIST`
+    FLIST="--folderrec '#Public/'"
 fi
 
-for folder in $FLIST;do
-    imapsync \
-        --host1 ${SERVER} \
-        --user1 ${USER} \
-        --authuser1 ${AUTHUSER} \
-        --password1 ${PASS} \
-        --authmech1 PLAIN \
-        --host2 ${SERVER} \
-        --user2 ${USER} \
-        --authuser2 ${AUTHUSER} \
-        --password2 ${PASS} \
-        --authmech2 PLAIN \
-        --syncinternaldates \
-        --syncacls \
-        --minage 365 \
-        --folder "#Public/$folder" \
-        --prefix1 "#Public" \
-        --prefix2 "#Archive" \
-        --expunge \
-        --delete \
-        --useheader 'Message-ID' \
-        --useheader 'Subject' >> $LOGFILE 2>&1
-    if [ $? != 0 ]; then
-        echo -n "Processing folder $folder: "
-        echo "FAILURE"
-    fi
-done 
+echo -n "Sync mail "
+
+imapsync \
+    --host1 ${SERVER} \
+    --user1 ${USER1} \
+    --authuser1 ${AUTHUSER} \
+    --password1 ${PASS} \
+    --authmech1 PLAIN \
+    --host2 ${SERVER} \
+    --user2 ${USER2} \
+    --authuser2 ${AUTHUSER} \
+    --password2 ${PASS} \
+    --authmech2 PLAIN \
+    --syncinternaldates \
+    --syncacls \
+    --minage 365 \
+    $FLIST \
+    --prefix1 "#Public" \
+    --prefix2 "#Archive" \
+    --exclude "Spam.*" \
+    --expunge \
+    --delete \
+    --useheader "Message-ID" \
+    --useheader "Subject" >> $LOGFILE 2>&1
+
+
+if [ $? != 0 ]; then
+    echo "FAILURE"
+else
+    echo "SUCCESS"
+fi
